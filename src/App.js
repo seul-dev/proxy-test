533b15051453ecc4dc1e0eb4daccffa60f5cba29
seul-dev <seul.dev@gmail.com>
2022-09-14 00:46:40 +0900

    local proxy 설정

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+  const URL =
+    '/api/ItemSearch.aspx?ttbkey=ttbls1051155001&Query=it&QueryType=Title&MaxResults=10&start=1&output=JS&Version=20131101';
+
+  const getData = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log(response.data.item);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>hi</p>
       </header>
     </div>
   );
